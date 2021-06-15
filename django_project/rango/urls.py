20ea6f6271a069d476ab0d08897641aebd2499b7
Akvilion <ianko.andrii@gmail.com>
2021-06-15 22:42:23 +0300

    implement addtional page + correct url to it

--- django_project/rango/urls.py
+++ django_project/rango/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import one
+from .views import one, about
 
 
 urlpatterns = [
-    path('', one),
+    path('', one, name='index'),
+    path('about/', about, name='about'),
 ]
--- django_project/rango/views.py
+++ django_project/rango/views.py
@@ -5,3 +5,8 @@ from django.shortcuts import render
 def one(request):
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
     return render(request, 'rango/index.html', context=context_dict)
+
+
+def about(request):
+    context_dict = {'about': 'about'}
+    return render(request, 'rango/about.html', context=context_dict)
