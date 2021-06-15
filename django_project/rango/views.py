9b3766eeff5c7ba85729581bdff5e28a5a87eb31
Akvilion <ianko.andrii@gmail.com>
2021-06-15 23:32:11 +0300

    '87'

--- django_project/rango/views.py
+++ django_project/rango/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
 
-def one(request):
+def index(request):
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
     return render(request, 'rango/index.html', context=context_dict)
 
