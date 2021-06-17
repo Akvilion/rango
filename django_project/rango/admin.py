d3e40600744bef661784217ff94b86d51a02abd7
Akvilion <ianko.adnrii@gmail.com>
2021-06-17 22:43:30 +0300

    implement prepopulated slug

--- django_project/rango/admin.py
+++ django_project/rango/admin.py
@@ -3,6 +3,9 @@ from .models import Category, Page
 
 # Register your models here.
 
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
 
-admin.site.register(Category)
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page)
