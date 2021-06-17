b576042eee9431b7467303232553a41bf90b1295
Akvilion <ianko.adnrii@gmail.com>
2021-06-17 23:02:00 +0300

    implement detail page with slug (107)

--- django_project/rango/urls.py
+++ django_project/rango/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, about
+from .views import index, about, show_category
 
 
 urlpatterns = [
     path('', index, name='index'),
     path('about/', about, name='about'),
+    path('category/<slug:category_name_slug>/', show_category, name='show_category'),
 ]
--- django_project/rango/views.py
+++ django_project/rango/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Category
+from .models import Category, Page
 
 
 def index(request):
@@ -14,3 +14,16 @@ def about(request):
     a=1
     context_dict = {'about': 'about'}
     return render(request, 'rango/about.html', context=context_dict)
+
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+    return render(request, 'rango/category.html', context=context_dict)
